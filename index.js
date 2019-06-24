b987434d26b6be12236fe6111f91530ec5e2673c
Logan Emerson <logemerson@gmail.com>
2019-06-24 19:48:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(origin, destination) {
+  if (origin > destination) {
+    return origin - destination;
+  } else {
+    return destination - origin;
+  }
+}
